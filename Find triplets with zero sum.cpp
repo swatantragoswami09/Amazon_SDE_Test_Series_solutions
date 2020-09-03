fc4feab0c3febfcaa4e2e6294c22f038245776b4
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-09-03 22:21:32 +0530

    Create Find triplets with zero sum.cpp

---
+++ Find triplets with zero sum.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+bool findTriplets(int arr[], int n);
+int main()
+{
+    int t;
+	cin>>t;
+	while(t--){
+    	int n;
+    	cin>>n;
+    	int arr[n]={0};
+    	for(int i=0;i<n;i++)
+    		cin>>arr[i];
+        if(findTriplets(arr, n))
+        cout<<"1"<<endl;
+        else 
+        cout<<"0"<<endl;
+	}
+    return 0;
+}// } Driver Code Ends
+/* You are required to complete the function below
+*  arr[]: input array
+*  n: size of array
+*/
+bool findTriplets(int arr[], int n)
+{ 
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                if(arr[i]+arr[j]+arr[k]==0)
+                return true;
+            }
+        }
+    }
+    return false;
+}
