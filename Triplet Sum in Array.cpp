e63992ffaf0e9544409517f8ab34aaa048cf40de
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-09-03 22:22:17 +0530

    Create Triplet Sum in Array.cpp

---
+++ Triplet Sum in Array.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+// function to find the triplet which sum to x
+bool find3Numbers(int A[], int n, int x)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                if(A[i]+A[j]+A[k]==x) return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int N,sum;
+		cin>>N>>sum;
+		int i,A[N];
+		for(i=0;i<N;i++)
+			cin>>A[i];
+        cout <<  find3Numbers(A, N, sum) << endl;
+    }
+}
+  // } Driver Code Ends
