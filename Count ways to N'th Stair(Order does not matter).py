cbb4dd8811b2c9dc9ea695592f3cae25c3348d2c
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-09-04 15:03:07 +0530

    Create Count ways to N'th Stair(Order does not matter).py

---
+++ Count ways to N'th Stair(Order does not matter).py
@@ -0,0 +1,21 @@
+    return (m//2 + 1)
+    # code here
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        m = int(input())
+        print(countWays(m))
+
+# } Driver Code Ends
