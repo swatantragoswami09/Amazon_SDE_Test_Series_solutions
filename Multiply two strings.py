af13f714bc899a75894caead40cec5cd6810dd2e
Swatantra <54662673+swatantragoswami09@users.noreply.github.com>
2020-09-04 10:39:44 +0530

    Create Multiply two strings.py

---
+++ Multiply two strings.py
@@ -0,0 +1,17 @@
+
+def multiply(a,b):
+    
+    print(str(int(a)*int(b)))
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        a,b=input().split()
+        multiply(a,b)
+# } Driver Code Ends
